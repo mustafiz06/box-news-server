b66d50e30d7dadd17fa36ed2b02b556b6e9f45f1
mustafiz06 <themustafiz06@gmail.com>
2023-07-16 04:28:28 +0600

    news,new-id,category-id API created

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express');
 const app = express();
-const port= process.env.PORT || 8000; 
+const port = process.env.PORT || 8000;
+const cors = require('cors');
+app.use(cors());
 
-const categories= require('./data/categories.json')
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
-app.get('/',(req,res)=>[
+app.get('/', (req, res) => {
     res.send("Hello World")
-])
-app.get('/news-categories',(req,res)=>[
+})
+app.get('/news-categories', (req, res) => {
     res.send(categories)
-])
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
 
-app.listen(port, ()=>{
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.filter(n => n.category_id === id);
+    if (id === "0") {
+        res.send(news)
+    } else {
+        if (!selectedNews) {
+            return res.status(404).send({ message: "News Not found" })
+        }
+        else {
+            res.send(selectedNews)
+        }
+    }
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    if (!selectedNews) {
+        return res.status(404).send({ message: "News Not found" })
+    }
+    else {
+        res.send(selectedNews)
+    }
+})
+
+app.listen(port, () => {
     console.log(`Server running on ${port}`);
 })
