079a3abb9650f169873878682e2bd3206602480e
mustafiz06 <themustafiz06@gmail.com>
2023-07-15 23:47:35 +0600

    express app create

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port= process.env.PORT || 8000; 
+
+const categories= require('./data/categories.json')
+
+app.get('/',(req,res)=>[
+    res.send("Hello World")
+])
+app.get('/news-categories',(req,res)=>[
+    res.send(categories)
+])
+
+app.listen(port, ()=>{
+    console.log(`Server running on ${port}`);
+})
